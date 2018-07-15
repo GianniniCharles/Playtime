a0bbc19a113f04c16314fb3ffca7d63a45f93bce
Giannini Charles <12gcharles@gmail.com>
2018-07-14 22:52:00 -0400

    Solved Reverse String

--- algoCasts/AlgoCasts/exercises/reversestring/index.js
+++ algoCasts/AlgoCasts/exercises/reversestring/index.js
@@ -6,6 +6,13 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+
+let arrayOfCharacters = str.split('');
+let reversedArrayOfCharacters = arrayOfCharacters.reverse();
+let reversedString = reversedArrayOfCharacters.join(''); 
+
+return reversedString;
+}
 
 module.exports = reverse;
