10d0c599caf79e1a229f8a857af787ccb708cd7d
Giannini Charles <12gcharles@gmail.com>
2018-08-11 15:06:48 -0400

    Initial Commit

---
+++ htmlDomPasswordPage/script.js
@@ -0,0 +1,35 @@
+const theCorrectPassword = "blah";
+const passwordAttempt = prompt("What is the password?");
+let coolThing = document.getElementById('theGoodStuff');
+
+passwordAttempt.confirm
+
+console.log('page activated');
+
+ function getTheGoodStuff(){
+
+
+console.log('CORRECT PASSWORD =',theCorrectPassword);
+console.log('ATTEMPTED PASSWORD =',passwordAttempt);
+
+
+
+
+
+
+  console.log('buttonclicked');
+    if (coolThing.style.display === "none"){
+      coolThing.style.display = "block";
+    } else {
+    coolThing.style.display = "none";
+    }
+
+  }
+
+
+
+
+
+
+
+getTheGoodStuff();
