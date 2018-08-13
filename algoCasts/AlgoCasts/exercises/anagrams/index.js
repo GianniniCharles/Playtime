c279eb019e1c9b94a0391994113371a41bc17592
Giannini Charles <12gcharles@gmail.com>
2018-08-12 22:53:09 -0400

    Completed anagrams & capitalize

--- algoCasts/AlgoCasts/exercises/anagrams/index.js
+++ algoCasts/AlgoCasts/exercises/anagrams/index.js
@@ -8,28 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-// My solution: Formed with help
+// Teacher solution 2: Uses regex and compares two solutions
 function anagrams(stringA, stringB) {
-  let charMapA = makeACharMap(stringA);
-  let charMapB = makeACharMap(stringB);
+  return cleanMyString(stringA) === cleanMyString(stringB);
+ 
+}
+
+
+function cleanMyString(str){
+  //this RegExp replaces all non letters with anything you want. In this case, we chose an empty string, which means replace with nothing.
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+}
+
+anagrams('Giannini', 'Giannini')
+module.exports = anagrams;
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------
+//My solution with help
+// // function anagrams(stringA, stringB) {
+//   let charMapA = makeACharMap(stringA);
+//   let charMapB = makeACharMap(stringB);
 
   
 
-  //checking to see if they have the same letters. Auto false if they dont.
-  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-   return false
-  } 
+//   //checking to see if they have the same letters. Auto false if they dont.
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//    return false
+//   } 
 
-//checking to see if the letters are used the same number of times in both.
-  for (let char in charMapA){
-    if (charMapA[char] !== charMapB[char]){
-      return false
-    } else {return true}
-  } //end loop
-
-} //end anagram
-
-
+// //checking to see if the letters are used the same number of times in both.
+//   for (let char in charMapA){
+//     if (charMapA[char] !== charMapB[char]){
+//       return false
+//     } 
+//   } //end loop
+//   return true
+// } //end anagram
 
 
 
@@ -37,45 +62,46 @@ function anagrams(stringA, stringB) {
 
 
 
-function makeACharMap(str) {
-const charMap = {};
-const theLowerCaseString = str.toLowerCase()
 
 
-  for (let char of theLowerCaseString) {
-      if (
-        char !== 'a'&&
-        char !== 'b'&&
-        char !== 'c'&&
-        char !== 'd'&&
-        char !== 'e'&&
-        char !== 'f'&&
-        char !== 'g'&&
-        char !== 'h'&&
-        char !== 'i'&&
-        char !== 'j'&&
-        char !== 'k'&&
-        char !== 'l'&&
-        char !== 'm'&&
-        char !== 'n'&&
-        char !== 'o'&&
-        char !== 'p'&&
-        char !== 'q'&&
-        char !== 'r'&&
-        char !== 's'&&
-        char !== 't'&&
-        char !== 'u'&&
-        char !== 'v'&&
-        char !== 'w'&&
-        char !== 'x'&&
-        char !== 'y'&&
-        char !== 'z') {continue};
+// function makeACharMap(str) {
+// const charMap = {};
+// const theLowerCaseString = str.toLowerCase()
 
-      charMap[char] = charMap[char]+1 || 1;
-  }
+// //helper function. You can also use regex
+//   for (let char of theLowerCaseString) {
+//       if (
+//         char !== 'a'&&
+//         char !== 'b'&&
+//         char !== 'c'&&
+//         char !== 'd'&&
+//         char !== 'e'&&
+//         char !== 'f'&&
+//         char !== 'g'&&
+//         char !== 'h'&&
+//         char !== 'i'&&
+//         char !== 'j'&&
+//         char !== 'k'&&
+//         char !== 'l'&&
+//         char !== 'm'&&
+//         char !== 'n'&&
+//         char !== 'o'&&
+//         char !== 'p'&&
+//         char !== 'q'&&
+//         char !== 'r'&&
+//         char !== 's'&&
+//         char !== 't'&&
+//         char !== 'u'&&
+//         char !== 'v'&&
+//         char !== 'w'&&
+//         char !== 'x'&&
+//         char !== 'y'&&
+//         char !== 'z') {continue};
 
-  //Loops INSIDE of the completed object using the keys
-  return charMap;
-}
+//       charMap[char] = charMap[char]+1 || 1;
+//   }
 
-module.exports = anagrams;
+//   //Loops INSIDE of the completed object using the keys
+//   return charMap;
+// }
+// -----------------------------------------------------
--- algoCasts/AlgoCasts/exercises/capitalize/index.js
+++ algoCasts/AlgoCasts/exercises/capitalize/index.js
@@ -7,6 +7,78 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+//My solution 2 based on teacher logic
+
+function capitalize(str) {
+  let theString = '';
+  
+  let splitStr = str.split('');
+  
+  for(let i=0; i<splitStr.length; i++) 
+  {//start for loop
+    if (splitStr[i-1] === ' '||(splitStr[i-1] === undefined)){ //start if resul
+    theString += splitStr[i].toUpperCase();
+  } //end if result
+  else {
+    theString += splitStr[i];
+  }//end if statement
+  
+  
+  }//end loop
+  
+  return(theString);
+
+  }//end function capitalize
+  
+
+
+
+
+
+
+  
+
+
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//   const words = [];
+
+//   for (let word of str.split(' ')){
+//     words.push(word[0].toUpperCase()+word.slice(1));
+//   }
+
+//   return words.join(' ');
+
+
+// }
+
+
+
+
+// My crazy solution
+// function capitalize(str) {
+
+//   let splitStr = str.split(' ');
+//   let capitalStrArray = []
+
+
+// for (let word of splitStr) {
+//   let correctWord = [];
+
+//   splitWord = word.split('');
+//   correctWord.push(splitWord[0].toUpperCase()+splitWord.splice(1).join(''));
+// capitalStrArray = capitalStrArray + ' ' +correctWord  
+// }
+
+
+// capitalStr = capitalStrArray.split(' ')
+// capitalStr.splice(0,1);
+// capitalizedString = capitalStr.join(' ');
+// return(capitalizedString);
+
+
+
+// }
