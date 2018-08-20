8d6bcf92097147239397f15a11ae210638da33c8
Giannini Charles <12gcharles@gmail.com>
2018-08-19 20:55:30 -0400

    Added Eloquent JS

--- algoCasts/AlgoCasts/exercises/steps/index.js
+++ algoCasts/AlgoCasts/exercises/steps/index.js
@@ -35,14 +35,14 @@
     for (let row = 0; row <n; row++){
       let stair = '';
 
-      for (let column = 0; column<n; column ++){
+      for (let column = 0; column<n; column++){
         if (column <= row){
           stair += '#';
         } else {
           stair += ' ';
         }
       }
-      console.log(stair);oe
+      console.log(stair);
     }
   }
 module.exports = steps;
