fe0ad0ef5dfdde342988249ce6f976df54b90b4a
Giannini Charles <12gcharles@gmail.com>
2018-08-21 21:47:57 -0400

    Reviewed some basic JS problems

--- eloquentJS/script.js
+++ eloquentJS/script.js
@@ -1,7 +1,44 @@
-let a = `Your bones don't break, mine do.\nThat's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on ${100/2}.`
+// let a = `Your bones don't break, mine do.\nThat's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on ${100/2}.`
 
-console.log(a);
+// console.log(a);
 
 //Escaping characters \
 
 //    \n makes a new line, \t indents, \" adds a quote as part of the string
+
+
+
+// -------------------------------------------------------
+//Eloquent JS Exercises Pg 37
+
+//1.
+
+function stairCase(){
+  let stairs = '';
+  for(let i = 0; i<7; i++){
+    stairs+='#';
+    console.log(stairs);
+  }
+
+}
+
+function fizzBuzz(n){
+  for(let i = 0; i<n; i++){
+
+    if (i%3 === 0 && i%5 === 0){
+      console.log('fizzBuzz');
+    } else if (i%5 ===0){
+      console.log('buzz');
+    } else if (i % 3 === 0){
+      console.log('fizz');
+    } else {
+      console.log(i);
+    }
+  }
+
+}
+
+stairCase();
+fizzBuzz(16);
+
+
