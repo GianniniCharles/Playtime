5b6a7ab4d6faa1e8b08d05e3a78342d385f5d740
Giannini Charles <12gcharles@gmail.com>
2018-08-27 23:45:12 -0400

    Working on staircase JS interview question

--- languages/JS/JSFoundationalExercises/eloquentJS/script.js
+++ languages/JS/JSFoundationalExercises/eloquentJS/script.js
@@ -40,5 +40,63 @@ function fizzBuzz(n){
 
 stairCase();
 fizzBuzz(16);
+// Chessboard --------------------------------------
+
+/*
+Pseudo code:
+
+1. create a string.
+  -max length of the string (64 characters)
+  -max length of line: 8 characters
+  -max lines: 8 lines.
+
+2. If line length is 8, insert a /n to create a new line.
+    reset the line counter to keep track of line completion.
+    use a loop to keep track of the total character count.
 
 
+3. On each iteration, print a space or a # based on conditional: It should start off with a space, then check the character before it.
+
+*/
+
+
+function chessboard(size){
+  let theChessBoard = '';
+  let line = 0;
+  for (let i =0; i < size**2; i++){
+
+     if (line === size) {
+      theChessBoard += '\n'
+      line = 0;
+    } //end conditional
+
+
+    if (theChessBoard[i - 1] === '\n') {
+      theChessBoard += theChessBoard[i - 2];
+      continue;
+    
+    }
+
+    if (theChessBoard[i - 1] === undefined || theChessBoard[i] === '#') {
+      theChessBoard += ' ';
+    } 
+
+    if (theChessBoard[i - 1] === ' ') {
+      theChessBoard += '#';
+    } 
+
+    // if (theChessBoard[i])
+  
+
+
+  line++;
+  console.log(theChessBoard[i])
+
+   
+  } //end loop
+
+
+console.log(theChessBoard);
+} //end function
+
+chessboard(8);
