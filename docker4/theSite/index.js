8e691211a02e275b0b30db4416f86ffd5f409cc7
Giannini Charles <12gcharles@gmail.com>
2018-09-10 15:39:17 -0400

    set up docker ip machine and database connection file, as well as index.js

---
+++ docker4/theSite/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const database = require ('./database');
+const app = express();
+
+
+console.log("Oh yeah!");
+
+database.initializeMongo();
+
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000')
+})
+
+
+app.get('/testFind', function(req, res){
+  database.Kitten.find(function(err, kittens){
+    if (err) return res.error(err);
+    console.log(kittens);
+    res.json(kittens);
+  })
+})
