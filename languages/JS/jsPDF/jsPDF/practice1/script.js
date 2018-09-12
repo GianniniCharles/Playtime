45927d3b974fc9615c43f2008a43fbfd8e3c2dc8
Giannini Charles <12gcharles@gmail.com>
2018-09-11 23:11:46 -0400

    Learned an impractical way of affecting styling on the pdf

--- languages/JS/jsPDF/jsPDF/practice1/script.js
+++ languages/JS/jsPDF/jsPDF/practice1/script.js
@@ -1,10 +1,23 @@
 function genPDF() {
   let doc = new jsPDF();
-  let theDiv = document.getElementById('testdiv');
-  console.log(theDiv);
+
+  let specialElementHandlers = {
+    '#hidediv': function(element,render) {return true;}
+  };
+  // let theDiv = document.getElementById('testdiv');
+    // console.log(theDiv);
+
+  // let theDiv2 = $('#testdiv');
+  // console.log(theDiv2);
+  
+  //NOTE: NORMAL DOM MANIPULATION DOES NOT WORK WITH THIS BECAUSE JQUERY PACKAGES THE ELEMENT INFORMATION MUCH DIFFERENTLY THAN DOM MANIPULATION.
+
   
 doc.fromHTML($('#testdiv').get(0), 20,20,{
-'width':500 });
+'width':500,
+'elementHandlers': specialElementHandlers
+
+});
 
 
 
